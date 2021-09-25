ad174593e7399bd3ce1c31ba1a3b55b4e6ec2e34
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2021-09-25 17:32:54 -0400

    capitalize

---
+++ capitalize/capitalizeindex.js
@@ -0,0 +1,14 @@
+//alternative method to capitalzize 
+  
+  function capitalize(str) {
+      const words = [];
+  
+      for (let word of str.split(' ')) {
+          words.push(word[0].toUpperCase() + word.slice(1));
+      }
+      return words.join(' ');
+    
+  }
+
+  module.exports = capitalize;
+  
